e0d4ff74931573f11123660c1fa42aa5f0b013c8
Alec Ostheimer <alecostheimer94@gmail.com>
2022-06-26 17:07:36 -0600

    udpated

--- stringPermutationChecking.ts
+++ stringPermutationChecking.ts
@@ -7,14 +7,26 @@ const isPermutationSortMethod = (str1: string, str2: string) => {
   return str1.split('').sort().join('') === str2.split('').sort().join('')
 }
 
-console.log(isPermutationSortMethod('abafds', 'baasdf'))
-
 const isPermutationCharCountingMethod = (str1: string, str2: string) => {
   if(str1.length !== str2.length) {
     return false
   }
 
+  let charCount = 0
 
+  for(let i = 0; i < str1.length; i++) {
+    charCount += str1.charCodeAt(i)
+  }
+
+  for(let i = str2.length - 1; i >= 0; i--) {
+    charCount -= str2.charCodeAt(i)
+
+    if(charCount < 0) {
+      return false
+    }
+  }
+
+  return charCount === 0
 }
 
-console.log('f'.localeCompare('d'))
+console.log(isPermutationCharCountingMethod('asdfae', 'fdsaad'))
